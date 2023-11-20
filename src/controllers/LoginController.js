880857112f093590f983bdc36890749ac4b2c58f
victornascimento <60269016+victornascimento22@users.noreply.github.com>
2023-11-20 14:50:26 -0300

    Update LoginController.js

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -2,9 +2,8 @@
 
 import LoginRequest from '/home/loadt/node/src/services/loginRequest.js';
 
-export async function post(req, res) { // Remova o '=>' após 'post'
-    const { username, password } = req.body; // Supondo que os parâmetros estejam presentes no corpo da requisição
-
+export async function post(req, res) { 
+    const { username, password } = req.body;
     try {
         const responseData = await LoginRequest(username, password);
         res.json({ data: responseData });
