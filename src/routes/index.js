061984ef3f6c792e3dc1f01b306cf226ac17c4ea
victornascimento <60269016+victornascimento22@users.noreply.github.com>
2023-11-20 14:50:10 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,12 @@ import express from 'express';
 
 const router = express.Router();
 
-// Define a rota GET
+
 router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Api Capital",
         version: "0.0.2"
     });
 });
-
-// Define a rota POST para login
 
 export default router;
